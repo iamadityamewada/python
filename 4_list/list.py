dc15ae87051804a62c1f065a336c427ad5288a48
Aditya Mewada <eng.adityamewada@gmail.com>
2024-10-07 18:23:44 +0530

    update

---
+++ 4_list/list.py
@@ -0,0 +1,34 @@
+# list container to store collection of any data types 
+list = ["aditya", "sun", 33, 3.5 , True]
+list[0] = "addi" # unlike string list is mutable 
+
+#list method
+#print(list.index(True)) # return the index of first matching element 
+
+# list.append("Harry") #append element last index of list 
+
+# ls2 = ["john", "jocob"]
+# list.extend(ls2)  # add element or iterables at the end of list 
+
+#list.insert(3, 44) # insert the element at a index without deleting the other one its update the position of after element by 1 index
+
+# print(list.pop())  # pop last element of list 
+# print(list.pop(1)) # pop the given index element 
+
+# print(list.remove(33)) # remove given element and return none 
+
+# print(list.clear()) # clear all element from list and return none
+
+# print(list.count(3)) # count the frequency of given value
+
+# list.reverse # reverse the element of list 
+
+# ls = [4,2,4,7,45,4,56,]
+# ls.sort()  # sort the list numeric in asscending order  and return none
+# print(ls)
+# ls.sort(reverse= True) #sort list in decennding order 
+# print(ls)
+
+
+
+print(list)
---
+++ techsimPy/basic.py
@@ -0,0 +1,14 @@
+# print("Input Function")
+# num1 = int(input("enter num1: "))
+# num2 = int(input("enter num2: "))
+# print(num1 + num2)
+
+# num1 = int(input("enter num1: "))
+# name = input("enter your name: ")
+# # for _ in range(0 , num1):
+# #     print(name)
+
+# print((name + "\t") *num1)
+
+cgpa = float(input("Enter your CGPA: "))
+print(cgpa*10)
